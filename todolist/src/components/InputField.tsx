b89d92abad9a90eabfdcec04007037d58bbae03e
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-11 21:23:32 -0700

    before making todo component

--- todolist/src/components/InputField.tsx
+++ todolist/src/components/InputField.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './styles.css';
 
-const InputField: React.FC = () => {
+interface TodoProps {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<TodoProps> = ({todo, setTodo} : TodoProps) => {
     return (
         <form className="input"> 
-            <input type="text" 
+            <input type="text"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)} 
                 placeholder="Your Task Here"
                 className="input-box"  />
             <button className="input-submit" type="submit">Add</button>
